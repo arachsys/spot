e871d3cc60b81a5cce16c0342ed05b0abe962978
Chris Webb <chris@arachsys.com>
2012-05-23 17:17:13 +0100

    Improve portability to BSD and Mac OS

--- despotify/sndqueue.h
+++ despotify/sndqueue.h
@@ -17,7 +17,8 @@
 #if defined(__linux__) 
 	#include <endian.h>
 #endif
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) | defined(__NetBSD__) || defined(__OpenBSD__) \
+      || defined(__DragonFly__) || defined(__APPLE__)
 	#include <machine/endian.h>
 	#define __BYTE_ORDER _BYTE_ORDER
 	#define __LITTLE_ENDIAN _LITTLE_ENDIAN
